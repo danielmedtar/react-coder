d248041ba2cf5c0467a00a14e80648d8d3b02089
danielmedtar <dmedinat.ar@gmail.com>
2021-12-08 22:36:11 -0300

    Segunda clase. Primera vista de JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Probando</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        //La etiqueta vacía se usa como fragment, para poder usar etiquetas repetidas como los párrafos de abajo
+        <>
+            <Header />
+        </>
+    ) 
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
+// Para traerse la librería de react desde Node y tenerla en Scope. Hoy día puede ser opcional
 import React from 'react';
+
+//Tener ReactDOM en scope
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Tener por lo menos un componente
+import App from "./App"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Mostrar la aplicación en el DOM
+ReactDOM.render(<App/>, document.getElementById('root'));
